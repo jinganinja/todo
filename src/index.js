7440ffccbcc14e3aa113090eb1e0e1999c47dc05
Micaela Simpson <micaelasimpson@Micaelas-MacBook-Pro.local>
2020-11-12 10:11:19 -0800

    connect todo input to api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const api = express();
+const bodyParser = require('body-parser');
 
 api.use(express.static(__dirname + '/public'));
+api.use(bodyParser.json());
 
 api.listen(3000, () => {
     console.log('API up and running!');
 });
 
-// api.get('/', (req, res) => {
-//     // console.log(req);
-//     // res.send('Hello, world!');
-//   });
+api.post('/add', (req, res) => {
+    console.log(req.body);
+    res.send('it works :)');
+})
